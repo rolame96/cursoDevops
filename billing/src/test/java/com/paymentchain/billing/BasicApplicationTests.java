5bb24c0851e3dcf3009152712a2431f90f7d4f1b
Rodrigo Lame <Rodrigo.Lame@endava.com>
2021-12-20 16:07:55 -0300

    fix test

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,12 +1,13 @@
 package com.paymentchain.billing;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest
+@SpringBootTest
 public class BasicApplicationTests {
 
 	@Test
